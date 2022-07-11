c17864f5bade4a3e6f78ca48ded7e26659157051
sena <snbyrktr@hotmail.com>
2022-07-11 18:33:12 +0300

    calculate

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
-var sena;
-console.log("sena");
-sena ="bayraktar";
-console.log(sena);
-var sena1='sss'
-console.log(sena1+'  '+sena);
+
+//vize final notunun ortalamasını hesaplayıp console'a yazdırma.
+ let name=prompt("adınızı giriniz: ")
+ let surname=prompt("soyadınızı giriniz: ")
+ let no=prompt("öğrenci numaranızı giriniz: ")
+ let class_name=prompt("ders adi giriniz: ")
+ let class1=prompt("vize notunuzu giriniz: ")
+ let class2=prompt("final notunuzu giriniz: ")
+ let sum=(Number(class1)+Number(class2))/2
+ console.log(no +" numaralı "+ name +" "+ surname+" isimli öğrencinin "+class_name+" dersinden aldığı ortalama not: " + sum +"dır");
+
+
    
